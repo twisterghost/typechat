ef8efb9cf145195062b81696ad7965dd3fb72bfa
Michael Barrett <Michael@AmpquotVPS.com>
2013-06-30 18:03:29 -0400

    Initial commit

--- hattip.js
+++ hattip.js
@@ -2,8 +2,10 @@ var express = require("express");
 var argv = require("optimist").argv;
 var app = express();
 var logger = require("winston");
-var socket = require("socket.io");
-var http = require("http");
+var server = require("http").createServer(app);
+var io = require('socket.io').listen(server);
+var fs = require("fs");
+var _ = require("underscore");
 
 logger.info("Welcome to Hat Tip!");
 
@@ -11,31 +13,132 @@ logger.info("Welcome to Hat Tip!");
 
 var port = varDefault(argv.port, process.env.PORT || 4488);
 var topic = varDefault(argv.topic, "Hat Tip");
-var links = [];
+var postMemory = [];
+var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
 
-logger.info("Using ports: " + port + " and " + (port + 1));
-logger.info("Using topic: " + topic);
+logger.info("Using port: " + port);
 
 
-// Routing.
+// Set app to use jade.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+// Set static directory.
+app.use(express.static(__dirname + '/public'));
+
+// Handle favicon requests.
+app.get("/favicon.ico", function(req, res) {
+  res.send("nope", 404);
+});
+
+// Default to index.
 app.get("/", function(req, res) {
-  res.render("index.jade", {topic: topic}, function(err, html) {
-    if (err) {
-      logger.error(err);
-      process.exit(1);
-    }
-    res.send(html);
+  res.render("index", {topic: topic});
+});
+
+// Start listening.
+server.listen(port);
+
+loadData();
+
+// Begin socket handling.
+io.sockets.on('connection', function (socket) {
+
+  socket.emit("postMemory", {posts: postMemory});
+
+  socket.on('my other event', function (data) {
+    console.log(data);
   });
+
+  socket.on("connect", function(data) {
+    console.log("Connected: " + data.username);
+    socket.set("username", fixName(data.username.trim()), function() {
+      socket.emit("connect-ack");
+    });
+  });
+
+  socket.on("send", function(data) {
+    console.log("Message sent by " + data.username);
+    parseMessage(data, socket);
+  })
 });
 
-app.get("/js/hattip-client.js", function(req, res) {
-  res.sendfile("js/hattip-client.js");
-});
 
-app.listen(port);
-socket.listen(port + 1);
+function parseMessage(data, socket) {
+
+  var post = {
+    type: "",
+    content: "",
+    author: data.username,
+    time: new Date().getTime() / 1000
+  }
+
+  if (data.message.trim()[0] == "!") {
+
+    // This is a comment.
+    // First, try to figure out if it is about an older link.
+
+    var firstSegment = data.message.trim().split(" ")[0];
+    if (firstSegment.match(/\!\-[0-9]+/)) {
+      var lookback = firstSegment.match(/[0-9]+/)[0];
+      data.message = data.message.replace(/\!\-[0-9]+/, "!");
+    }
+
+    post.lookback = lookback;
+    post.type = "comment";
+    post.content = _.escape(data.message.trim().substring(1).trim());
+    postMemory.push(post);
+
+  } else if (data.message.trim()[0] == "^") {
+
+    // This is a name change.
+    post.type = "namechange";
+    var newName = fixName(data.message.trim().substring(1));
+    post.author += " -> " + newName;
+    post.content = data.username + " has changed their name to " + newName;
+    postMemory.push(post);
+
+    socket.emit("namechange", {newName: newName});
+
+  } else if (data.message.trim()[0] == "#") {
+    //setEnterAction("Search for '" + data.message.trim().substring(1).trim() + "'");
+  } else if (data.message.trim().match(urlPattern) !== null) {
+
+    // This is a link.
+    post.type = "link";
+    post.content = data.message.trim();
+    postMemory.push(post);
+
+  } else {
+    socket.emit("unknown");
+    return;
+  }
+
+  socket.emit("new", post);
+  socket.broadcast.emit("new", post);
+  saveData();
+}
 
 // Helper functions.
 function varDefault(variable, defaultValue) {
   return typeof(variable) !== 'undefined' ? variable : defaultValue;
+}
+
+function saveData() {
+  fs.writeFileSync("memory", JSON.stringify(postMemory));
+}
+
+function loadData() {
+  if (fs.existsSync("memory")) {
+    postMemory = JSON.parse(fs.readFileSync("memory"));
+  }
+}
+
+function sanitize(string) {
+  return _.escape(string.trim());
+}
+
+function fixName(string) {
+  string = sanitize(string);
+  return string.substring(0, 30);
 }
---
+++ public/js/hattip-client.js
@@ -0,0 +1,179 @@
+var state = {
+  status: "start"
+}
+
+var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+
+var socket = io.connect("http://" + location.host);
+
+socket.on("connect-ack", function(data) {
+  joinHatTip();
+});
+
+socket.on("postMemory", function(data) {
+  for (var i = 0; i < data.posts.length; i++) {
+    addContent(data.posts[i]);
+  }
+});
+
+socket.on("new", function(data) {
+  addContent(data);
+});
+
+socket.on("namechange", function(data) {
+  state.username = data.newName;
+});
+
+function addContent(data) {
+  if (state.status == "connected") {
+    $("#mainInput").attr("placeholder", "Paste or type...");
+  }
+  var add = "";
+
+  if (data.type == "comment") {
+    add = $("<div class='addition'><div class='comment'>" + data.content + "</div>" +
+      "<div class='author'>" + data.author + "</div></div>");
+
+    if (data.lookback) {
+      if ($(".toplevel").size() > parseInt(data.lookback)) {
+        $($(".toplevel").get(parseInt(data.lookback))).append(add);
+      } else {
+        $(".toplevel").first().append(add);
+      }
+    } else {
+      $(".toplevel").first().append(add);
+    }
+
+  } else if (data.type == "namechange") {
+    add = $("<div class='addition'><div class='nameChange'>" + data.content + "</div>" +
+      "<div class='author'>" + data.author + "</div></div>");
+    $(".toplevel").first().append(add);
+
+  } else if (data.type == "link") {
+    add = $("<div class='addition toplevel'><div class='link'>" +
+     "<a target='_blank' href='" + data.content + "'>" + data.content + "</a></div>" +
+      "<div class='author'>" + data.author + "</div></div>");
+    $("#content").prepend(add);
+  }
+
+}
+
+socket.on("unknown", function() {
+  $("#mainInput").attr("placeholder", "Give a link, !..., ^... or #...");
+});
+
+$(document).ready(function() {
+
+  $("html").click(function() {
+    $("#mainInput").focus();
+  });
+
+  // Animation for action div
+  $("#mainInput").keyup(function(e) {
+
+    var input = $(this);
+
+    $("#enterAction").stop(true);
+
+    if ($(this).val().trim() != "") {
+      if (state.status == "start" || state.status == "connecting") {
+        setEnterAction("Join This HatTip");
+      } else {
+
+        if (input.val().trim()[0] == "!") {
+
+          if (input.val().match(/\!\-[0-9]+/)) {
+            console.log("oi");
+            var lookback = input.val().match(/[0-9]+/)[0];
+            if ($(".toplevel").size() > parseInt(lookback)) {
+              var link = $($(".toplevel a").get(parseInt(lookback))).html();
+              setEnterAction("Comment about " + link);
+              return;
+            }
+          }
+
+          var link = $(".toplevel a").first().html();
+          setEnterAction("Comment about " + link);
+        } else if (input.val().trim()[0] == "^") {
+          setEnterAction("Update Name");
+        } else if (input.val().trim()[0] == "#") {
+          setEnterAction("Search for '" + input.val().trim().substring(1) + "'");
+        } else if (input.val().trim().match(urlPattern) !== null) {
+          setEnterAction("Post Link");
+        } else {
+          $("#enterAction").animate({
+            opacity: 0
+          }, 500);
+          return;
+        }
+      }
+
+      $("#enterAction").animate({
+        opacity: .5
+      }, 500);
+    } else {
+      $("#enterAction").animate({
+        opacity: 0
+      }, 500);
+    }
+  });
+
+  // Enter actions.
+  $("#mainInput").keypress(function(e) {
+    if ( e.keyCode == 13 && $("#mainInput").val().trim() != "" ) {
+      enter();
+    }
+  });
+
+});
+
+function enter() {
+  var input = $("#mainInput");
+  var text = input.val();
+
+  // Connect if we have yet to.
+  if (state.status == "start") {
+    input.val("Connecting...");
+    state.status = "connecting";
+    input.attr("disabled", "true");
+    connect(text);
+    return;
+  }
+
+  // Otherwise, send a mesage.
+  input.val("");
+  socket.emit("send", {
+    username: state.username,
+    message: text
+  })
+
+}
+
+function connect(input) {
+  state.username = input;
+  socket.emit("connect", {
+    username: input
+  });
+}
+
+function joinHatTip() {
+  state.status = "connected";
+  $("#enterAction").html("Welcome, " + state.username);
+  setTimeout(showHelp, 1000);
+  $("#mainInput").removeAttr("disabled");
+  $("#mainInput").attr("placeholder", "Paste or type...");
+  $("#mainInput").val("");
+  $("#mainInput").animate({
+    fontSize: "5em"
+  }, 500);
+}
+
+function setEnterAction(action) {
+  $("#enterAction").html(action + "&crarr;")
+}
+
+function showHelp() {
+  $("#enterAction").html("Share links. Use ! to comment, ^ to change names.").animate({
+    opacity: .5
+  }, 500);
+}
